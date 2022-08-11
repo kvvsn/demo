c296980b854fee9eb67f6678fb70c02a22ea5daa
kvvsn <satyanarayana91770@gmail.com>
2022-08-11 07:17:52 +0530

    initial comt

--- file1.py
+++ file1.py
@@ -1,1 +1,14 @@
+
+list_a=[1,2,3,4,5,6,7,8]
+print(list_a)
+print(type(list_a))
+tuple_b=(1,2,3,4,5,6,7,8,9)
+print(tuple_b)
+print(type(tuple_b))
+Tuple_b={1,2,3,4,5,6,7,8,9,10}
+print(Tuple_b)
+print(type(Tuple_b))
+dic_d={'1':'asha','2':'bsha','3':'csha'}
+print(dic_d)
+print(type(dic_d))
 print ("haii")
